1b7ee54e16491e8fce9974e57c552a85621aab6c
Volkan Şah <VolkanSah@users.noreply.github.com>
2024-07-10 12:40:25 +0200

    Create WebServer.java

---
+++ src/com/WebServer.java
@@ -0,0 +1,27 @@
+package com.example.hiddenserviceapp;
+
+import fi.iki.elonen.NanoHTTPD;
+
+import java.io.IOException;
+
+public class WebServer extends NanoHTTPD {
+
+    public WebServer() {
+        super(8080);
+    }
+
+    @Override
+    public Response serve(IHTTPSession session) {
+        String msg = "<html><body><h1>Hello, World!</h1></body></html>";
+        return newFixedLengthResponse(msg);
+    }
+
+    public void start() {
+        try {
+            start(NanoHTTPD.SOCKET_READ_TIMEOUT, false);
+            System.out.println("Web server started on port 8080");
+        } catch (IOException ioe) {
+            System.err.println("Couldn't start server:\n" + ioe);
+        }
+    }
+}
